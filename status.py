3fb25958d22d9ff6efcfd8c6a56f35b2afcb5640
khoa <khoa.n997@gmail.com>
2023-08-09 19:52:15 -0400

    applied click to focus patch

--- status.py
+++ status.py
@@ -26,7 +26,7 @@ def get_temperature_from_sensors():
 
 
 def get_weather_info(location=""):
-    url = f"https://wttr.in/{location}?format=%C|%t"
+    url = f"https://wttr.in?format=%C|%t"
     response = requests.get(url)
     conditions, temp = response.text.split('|')
     keyword = extract_keyword(conditions.strip())
@@ -37,21 +37,21 @@ def get_weather_icon(keyword):
     icon_map = {
         'clear': '\uf00d',
         'sunny': '\uf00d',
-        'cloudy': '\uf0c2',
-        'overcast': '\uf0c2',
-        'mist': '\uf0c5',
-        'rain': '\uf019',
+        'cloudy': '\uf041',
+        'overcast': '\uf00c',
+        'mist': '\uf021',
+        'rain': '\uf018',
         'snow': '\uf01b',
-        'sleet': '\uf0c2',    # Using cloud as there isn't a direct icon for sleet in FontAwesome 4
-        'drizzle': '\uf019',
-        'thunder': '\uf01e',
-        'blowing': '\uf063',  # Wind icon for blowing snow
-        'blizzard': '\uf01b',
+        'sleet': '\uf0b5',    # Using cloud as there isn't a direct icon for sleet in FontAwesome 4
+        'drizzle': '\uf01a',
+        'thunder': '\uf016',
+        'blowing': '\uf011',  # Wind icon for blowing snow
+        'blizzard': '\uf064',
         'fog': '\uf014',
-        'freezing': '\uf017',  # Using moon for freezing conditions (no direct match)
+        'freezing': '\uf076',  # Using moon for freezing conditions (no direct match)
         'ice': '\uf076',      # Using asterisk for ice pellets
-        'shower': '\uf019',
-        'torrential': '\uf019',
+        'shower': '\uf01c',
+        'torrential': '\uf01c',
     }
     return icon_map.get(keyword, '\uf07b')  # Default: FontAwesome 
 
@@ -64,6 +64,7 @@ def extract_keyword(conditions):
     ]
     for pattern in patterns:
         if re.search(pattern, conditions, re.I):  # re.I makes the search case-insensitive
+            print(pattern)
             return pattern
     return None
 
@@ -73,13 +74,13 @@ def print_statusbar():
     while True:
         current_time_str = getTime()
         cpu_temp = get_temperature_from_sensors()
-        weather_info = get_weather_info()
+      
         if elapsed_minutes >= 10:
             weather_info = get_weather_info()
             elapsed_minutes = 0  # Reset the counter
 
         if cpu_temp is not None:
-            set_dwm_status(f"  \uf017  { current_time_str} | \uf2db {cpu_temp}°C | {weather_info}")
+            set_dwm_status(f"  \uf092  { current_time_str} | \uf2db {cpu_temp}°C | {weather_info}")
       
 
         sleep_time = 60 - time.localtime().tm_sec
